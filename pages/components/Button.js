0b94c644332fa80a9b351b90f70c89bc100dbc0b
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-20 20:18:15 +0700

    Update Button.js

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,11 +1,16 @@
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
-export default function Button(props) {
-    return(
+const Button = (props) => {
+  return (
+    <div>
         <Link href={props.url} target="_blank">
         <div className="flex items-center text-center hover:text-white/60 trasition-all p-4 bg-black text-white font-light">
             <p>{props.title} <span className="font-bold">{props.span}</span></p>
         </div>
         </Link>
-    )
+    </div>
+  )
 }
+
+export default Button
