69fe02c065e83172146fa5d348920c0885372f56
Bryannn <107366548+bryansdream@users.noreply.github.com>
2022-11-20 20:18:10 +0700

    Update TextLink.js

--- pages/components/TextLink.js
+++ pages/components/TextLink.js
@@ -1,11 +1,16 @@
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
-export default function TextLink(props) {
-    return(
+const TextLink = (props) => {
+  return (
+    <div>
         <Link href={props.url}>
         <div className="font-light mt-1.5 underline-offset-4 text-white">
             <p>{props.title}<span className="font-bold">{props.span}</span></p>
         </div>
         </Link>
-    )
+    </div>
+  )
 }
+
+export default TextLink
